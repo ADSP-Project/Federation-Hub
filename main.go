01aa6ac43405ca79caf36ac7fe12472968ba0733
Denis Koshelev <truemacintoshchannel@gmail.com>
2023-05-30 19:58:25 +0200

    Add version with databases

--- main.go
+++ main.go
@@ -1,38 +1,74 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"database/sql"
 	"encoding/json"
 	"net/http"
-	"sync"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type Shop struct {
 	Name string `json:"name"`
 }
 
-var shopList = struct{
-    sync.RWMutex
-    shops []Shop
-}{}
+var db *sql.DB
 
 func getShops(w http.ResponseWriter, r *http.Request) {
-    shopList.RLock()
-    defer shopList.RUnlock()
-	json.NewEncoder(w).Encode(shopList.shops)
+	rows, err := db.Query("SELECT name FROM shops")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var shops []Shop
+	for rows.Next() {
+		var shop Shop
+		if err := rows.Scan(&shop.Name); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		shops = append(shops, shop)
+	}
+
+	json.NewEncoder(w).Encode(shops)
 }
 
 func addShop(w http.ResponseWriter, r *http.Request) {
 	var newShop Shop
 	json.NewDecoder(r.Body).Decode(&newShop)
-    shopList.Lock()
-    shopList.shops = append(shopList.shops, newShop)
-    shopList.Unlock()
+
+	_, err := db.Exec("INSERT INTO shops (name) VALUES ($1)", newShop.Name)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	json.NewEncoder(w).Encode(newShop)
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	db, err = sql.Open("postgres", fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", dbUser, dbPassword, dbName))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	router := mux.NewRouter()
 	router.HandleFunc("/shops", getShops).Methods("GET")
 	router.HandleFunc("/shops", addShop).Methods("POST")
